447ba7188d3d6eb00ddaacd818b5eca7759f1de0
guleao <guztavusz@gmail.com>
2024-06-04 15:49:12 -0300

    Aula 7 Componentes em Next ( Realizei as outras mas não comitei )

---
+++ app/blog/page.js
@@ -0,0 +1,12 @@
+export default function Blog() {
+    return (
+        <>
+        <h1>Página do Blog</h1>
+        <ul>
+            <li>
+                <Link href="/page">Inicio</Link>
+            </li>
+        </ul>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link href="/blog">
+                        Blog
+                    </Link>
+                </li>
+               
+            </ul>
+            <h1>Hello World Next.js</h1>
+        </div>
+    )
 }
